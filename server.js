46b05da6a0703c4e838fa80a22b795d4d4d2d837
Ajay Giri <ajaygir37m@gmail.com>
2026-02-23 01:25:02 +0530

    fix mysql env

--- server.js
+++ server.js
@@ -17,8 +17,8 @@ const db = mysql.createPool({
   password: process.env.MYSQLPASSWORD,
   database: process.env.MYSQLDATABASE,
   port: process.env.MYSQLPORT,
-  waitForConnections:true,
-  connectionLimit:10
+  waitForConnections: true,
+  connectionLimit: 10
 });
 
 /* ======================
